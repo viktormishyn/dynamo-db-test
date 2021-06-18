0994b50ef2f0409a8b4be2181a4b8ee1629c6e7b
viktormishyn <viktormishyn@gmail.com>
2021-06-18 12:10:33 +0300

    Add index.js with create table script

---
+++ index.js
@@ -0,0 +1,33 @@
+// Create table
+
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "eu-central-1",
+  endpoint: "http://localhost:8000",
+});
+
+var dynamodb = new AWS.DynamoDB();
+var params = {
+  TableName: "NodeJsBaseballStats",
+  KeySchema: [
+    { AttributeName: "TeamID", KeyType: "HASH" }, // PartitionKey
+    { AttributeName: "SK", KeyType: "RANGE" }, //SortKey
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "TeamID", AttributeType: "S" },
+    { AttributeName: "SK", AttributeType: "S" },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5,
+  },
+};
+
+dynamodb.createTable(params, function (err, data) {
+  if (err) {
+    console.log("Unable to create table!", JSON.stringify(err));
+  } else {
+    console.log("Table created!");
+  }
+});
